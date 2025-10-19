0ceef5ccdead164c95a5aeec1c21a0010e433f7b
tomomi mizukami <ff10mm11yy23tamk@gmail.com>
2025-10-19 21:25:23 +0900

    feat: ロゴ画像のインポートを追加し、スタイルを更新

--- app/_components/Logo.js
+++ app/_components/Logo.js
@@ -1,11 +1,22 @@
 import Image from "next/image";
 import Link from "next/link";
+import logo from "@/public/logo.png";
 
 function Logo() {
   return (
     <Link href="/" className="flex items-center gap-4 z-10">
-      <Image src="/logo.png" height="60" width="60" alt="The Wild Oasis logo" />
-      <span className="text-xl font-semibold">The Wild Oasis</span>
+      {/* <Image src="/logo.png" height="60" width="60" alt="The Wild Oasis logo" /> */}
+      <Image
+        src={logo}
+        height="60"
+        // imageのクオリティを指定するための属性
+        quality={100}
+        width="60"
+        alt="The Wild Oasis logo"
+      />
+      <span className="text-xl font-semibold text-primary-100">
+        The Wild Oasis
+      </span>
     </Link>
   );
 }
