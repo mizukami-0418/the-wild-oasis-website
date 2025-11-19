fe1a91c8d43ad03c47f7cb52ad3f69209c756454
tomomi mizukami <ff10mm11yy23tamk@gmail.com>
2025-11-19 20:53:29 +0900

    feat: キャビン、予約、日付選択コンポーネントを追加

---
+++ app/_components/Reservation.js
@@ -0,0 +1,22 @@
+import { ca } from "date-fns/locale";
+import { getBookedDatesByCabinId, getSettings } from "../_lib/data-service";
+import DateSelector from "./DateSelector";
+import ReservationForm from "./ReservationForm";
+
+export default async function Reservation({ cabin }) {
+  const [settings, bookedDates] = await Promise.all([
+    getSettings(),
+    getBookedDatesByCabinId(cabin.id),
+  ]);
+
+  return (
+    <div className="grid grid-cols-[auto_auto] border border-primary-800 min-h-[100px]">
+      <DateSelector
+        settings={settings}
+        bookedDates={bookedDates}
+        cabin={cabin}
+      />
+      <ReservationForm cabin={cabin} />
+    </div>
+  );
+}
--- app/cabins/[cabinId]/page.js
+++ app/cabins/[cabinId]/page.js
@@ -1,7 +1,15 @@
-import TextExpander from "@/app/_components/TextExpander";
-import { getCabin, getCabins } from "@/app/_lib/data-service";
+import Cabin from "@/app/_components/Cabin";
+import Reservation from "@/app/_components/Reservation";
+import Spinner from "@/app/_components/Spinner";
+import {
+  getBookedDatesByCabinId,
+  getCabin,
+  getCabins,
+  getSettings,
+} from "@/app/_lib/data-service";
 import { EyeSlashIcon, MapPinIcon, UsersIcon } from "@heroicons/react/24/solid";
 import Image from "next/image";
+import { Suspense } from "react";
 
 export async function generateMetadata({ params }) {
   const { name } = await getCabin(params.cabinId);
@@ -19,61 +27,20 @@ export async function generateStaticParams() {
 
 export default async function Page({ params }) {
   const cabin = await getCabin(params.cabinId);
-  const { id, name, maxCapacity, regularPrice, discount, image, description } =
-    cabin;
 
   return (
     <div className="max-w-6xl mx-auto mt-8">
-      <div className="grid grid-cols-[3fr_4fr] gap-20 border border-primary-800 py-3 px-10 mb-24">
-        <div className="relative scale-[1.15] -translate-x-3">
-          <Image
-            src={image}
-            fill
-            className="object-cover"
-            alt={`Cabin ${name}`}
-          />
-        </div>
-
-        <div>
-          <h2 className="text-accent-100 font-black text-4xl mb-5 translate-x-[-254px] bg-primary-950 p-6 pb-1 w-[150%]">
-            The Wild Oasis <br />
-            {name}
-          </h2>
-
-          <p className="text-lg text-primary-300 mb-10">
-            <TextExpander>{description}</TextExpander>
-          </p>
-
-          <ul className="flex flex-col gap-4 mb-7">
-            <li className="flex gap-3 items-center">
-              <UsersIcon className="h-5 w-5 text-primary-600" />
-              <span className="text-lg">
-                <span className="font-bold">{maxCapacity}</span>{" "}
-                名様まで宿泊可能
-              </span>
-            </li>
-            <li className="flex gap-3 items-center">
-              <MapPinIcon className="h-5 w-5 text-primary-600" />
-              <span className="text-lg">
-                <span className="font-bold">中央アルプス</span>{" "}
-                (日本)の中心部に位置しています。
-              </span>
-            </li>
-            <li className="flex gap-3 items-center">
-              <EyeSlashIcon className="h-5 w-5 text-primary-600" />
-              <span className="text-lg">
-                プライバシーは<span className="font-bold">100%</span> 保証
-              </span>
-            </li>
-          </ul>
-        </div>
-      </div>
+      <Cabin cabin={cabin} />
 
       <div>
-        <h2 className="text-5xl font-semibold text-center">
-          Reserve today. Pay on arrival.
+        <h2 className="text-5xl font-semibold text-center mb-10 text-accent-400">
+          この部屋を予約する。代金は到着時にお支払いください。
         </h2>
       </div>
+      {/* <div className="grid grid-cols-2 border border-primary-800 min-h-[400px]"> */}
+      <Suspense fallback={<Spinner />}>
+        <Reservation cabin={cabin} />
+      </Suspense>
     </div>
   );
 }
