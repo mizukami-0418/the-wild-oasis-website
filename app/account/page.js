590a5fbbac84ee9e0a2c79449c2d84cf3b08a2f6
tomomi mizukami <ff10mm11yy23tamk@gmail.com>
2025-12-08 20:32:37 +0900

    feat: ログインページを作成し、サインインボタンを追加

--- app/account/page.js
+++ app/account/page.js
@@ -1,9 +1,15 @@
+import { auth } from "../_lib/auth";
+
 export const metadata = {
   title: "Guest Area",
 };
 
-export default function Page() {
+export default async function Page() {
+  const session = await auth();
+  const firstName = session.user.name.split(" ")[0];
   return (
-    <h2 className="font-semibold text-2xl text-accent-400 mb-7">Guest Area</h2>
+    <h2 className="font-semibold text-2xl text-accent-400 mb-7">
+      ようこそ！{firstName}さん😃
+    </h2>
   );
 }
