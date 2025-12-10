7889d3a1e6b8ee648e5fff28b3a25146f1cd8fab
tomomi mizukami <ff10mm11yy23tamk@gmail.com>
2025-12-10 21:55:14 +0900

    feat: プロフィール更新フォームにゲスト情報を追加

--- app/_lib/actions.js
+++ app/_lib/actions.js
@@ -1,6 +1,34 @@
 "use server";
 
-import { signIn, signOut } from "./auth";
+import { auth, signIn, signOut } from "./auth";
+import { supabase } from "./supabase";
+
+export async function updateGuest(formData) {
+  const session = await auth();
+  if (!session) {
+    throw new Error("ログインしてね 😄");
+  }
+
+  const nationalID = formData.get("nationalID");
+  const [nationality, countryFlag] = formData.get("nationality").split("%");
+
+  if (!/^[a-zA-Z0-9]{6,12}$/.test(nationalID)) {
+    throw new Error(
+      "有効な国民ID番号を入力してください (6〜12文字の英数字) 😄"
+    );
+  }
+
+  const updateData = { nationalID, nationality, countryFlag };
+
+  const { data, error } = await supabase
+    .from("guests")
+    .update(updateData)
+    .eq("id", session.user.guestId);
+
+  if (error) {
+    throw new Error("プロフィールを更新できませんでした 😄");
+  }
+}
 
 export async function signInAction() {
   await signIn("google", { redirectTo: "/account" });
