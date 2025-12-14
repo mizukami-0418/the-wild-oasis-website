1c5f29354c7d329dfe1cbc76a16aca7bb52179b8
tomomi mizukami <ff10mm11yy23tamk@gmail.com>
2025-12-14 17:48:06 +0900

    feat: 予約編集機能を追加し、ボタンをコンポーネント化

--- app/_lib/actions.js
+++ app/_lib/actions.js
@@ -3,6 +3,8 @@
 import { revalidatePath } from "next/cache";
 import { auth, signIn, signOut } from "./auth";
 import { supabase } from "./supabase";
+import { getBookings } from "./data-service";
+import { redirect } from "next/navigation";
 
 export async function updateGuest(formData) {
   const session = await auth();
@@ -56,6 +58,46 @@ export async function deleteReservation(bookingId) {
     throw new Error("予約を削除できませんでした 😄");
   }
   revalidatePath("/account/reservations");
+}
+
+export async function updateBooking(formdata) {
+  const bookingId = Number(formdata.get("bookingId"));
+
+  // 認証の確認
+  const session = await auth();
+  if (!session) {
+    throw new Error("ログインしてね 😄");
+  }
+  // 認可の確認
+  const guestBookings = await getBookings(session.user.guestId);
+  const guestBookingIds = guestBookings.map((booking) => booking.id);
+  if (!guestBookingIds.includes(bookingId)) {
+    throw new Error("この予約を更新する権限がありません 😄");
+  }
+  // 更新データの準備
+  const updateData = {
+    numGuests: Number(formdata.get("numGuests")),
+    observations: formdata.get("observations").slice(0, 500),
+  };
+
+  // データベースの更新
+  const { error } = await supabase
+    .from("bookings")
+    .update(updateData)
+    .eq("id", bookingId)
+    .select()
+    .single();
+
+  // エラー処理
+  if (error) {
+    throw new Error("予約を更新できませんでした 😄");
+  }
+
+  // キャッシュの再検証
+  revalidatePath(`/account/reservations/edit/${bookingId}`);
+
+  // 処理後のリダイレクト
+  redirect("/account/reservations");
 }
 
 export async function signInAction() {
